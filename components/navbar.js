5cf405f4ca8910d97ec94cd8b72bd0d373134096
Ethan Russell <44298283+Ethan-Russell@users.noreply.github.com>
2023-03-19 00:23:46 -0400

    Clean up navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,39 +1,11 @@
 import {NavLink} from './navlink';
-import {useRouter} from 'next/router';
 
-import styles from '../styles/Navbar.module.css'
+export default Navbar;
 
-// export const navLinks = [
-//   { 
-//     name: "Home", 
-//     path: "/" 
-//   },
-//   {
-//     name: "Posts",
-//     path: "/posts",
-//   },
-//   {
-//     name: "Recipes",
-//     path: "/recipes",
-//   },
-// ];
-
-
-// const NavItem = ({ item }) => {
-//   const router = useRouter();
-//   return <>{router.pathname === "/" ? item : ""}</>;
-// };
-
-// // proptypes check
-// NavItem.propTypes = {
-//   item: propTypes.string,
-// };
-
-
-const Navbar = () => {  
+Navbar = () => {  
   return (
     <nav className='topNav'>
-      <NavLink href='/' className="nav-item">
+      <NavLink href='/' exact className="nav-item">
         Home
       </NavLink>
       <NavLink href='/posts' className="nav-item">
@@ -43,18 +15,5 @@ const Navbar = () => {
         Recipes
       </NavLink>
     </nav>
-    // <nav>
-    //   {navLinks.map((link, index) => {
-    //     return (
-    //       <ul>
-    //         {/* <Link href={link.path} className={currentRoute === link.path ? 'active' : 'nonActive'}> */}
-    //         <Link href={link.path} className={styles.active}>
-    //           {link.name}
-    //         </Link>
-    //       </ul>
-    //     );
-    //   })}
-    // </nav>
   );
 }
-export default Navbar;
